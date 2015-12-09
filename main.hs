44ecd70ce452f3de4b4b396ecdd535ba67301e47
Stanislav Klenin <stanislav.klenin@gmail.com>
2015-12-09 19:43:37 +0300

    reading configuration from first command line argument

--- main.hs
+++ main.hs
@@ -4,8 +4,10 @@ import Control.Exception    (bracket)
 import Control.Monad        (msum)
 import Data.Acid            (AcidState, openLocalState)
 import Data.Acid.Local      (createCheckpointAndClose)
-import Data.Text            (pack, empty)
+import Data.Configurator    (Worth(..), load, lookupDefault)
+import Data.Text            (Text, pack, empty)
 import Happstack.Server
+import System.Environment   (getArgs)
 import Text.Lucius          (renderCss)
 import Web.Routes.Happstack (implSite)
 
@@ -13,18 +15,39 @@ import Routes
 import Render               (stylesheet)
 import Storage
 
-routes :: AcidState Board -> ServerPart Response
-routes acid =
-    do decodeBody (defaultBodyPolicy "/tmp/" 0 1000000 1000000)
-       msum [ do dirs "style.css" $ nullDir
-                 setHeaderM "Content-Type" "text/css"
-                 ok $ toResponse $ renderCss stylesheet
-            , implSite (pack "http://localhost:8000") empty (site acid)
-            , notFound $ toResponse (pack "not found\n")
-            ]
+routes :: AcidState Board -> BodyPolicy -> Text -> ServerPart Response
+routes acid decodePolicy homeUrl = do
+    decodeBody decodePolicy
+    msum [ do dirs "style.css" $ nullDir
+              setHeaderM "Content-Type" "text/css"
+              ok $ toResponse $ renderCss stylesheet
+         , implSite homeUrl empty (site acid)
+         , notFound $ toResponse (pack "not found\n")
+         ]
 
 main :: IO ()
-main =
+main = do
+    args   <- getArgs
+    config <- load [ Optional (head' args) ]
+    tmpdir <- lookupDefault "/tmp" config (pack "tmp")
+    h      <- lookupDefault "localhost" config (pack "host")
+    p      <- lookupDefault (8000 :: Int) config (pack "port")
+    
+    -- TODO: other policy parameters must be configurable too
+    let policy = defaultBodyPolicy tmpdir 0 1000000 1000000
+        home   = pack $ appRoot h p
+        conf   = nullConf { port = p }
+    
     bracket (openLocalState initialBoardState)
             (createCheckpointAndClose)
-                (\acid -> simpleHTTP nullConf (routes acid))
+                (\acid -> simpleHTTP conf (routes acid policy home))
+    where
+        head' :: [String] -> String
+        head' [] = ""
+        head' (x:_) = x
+        
+        appRoot h p =
+            case p of
+                80  -> "http://" ++ h ++ "/"
+                443 -> "https://" ++ h ++ "/"
+                _   -> "http://" ++ h ++ ":" ++ (show p) ++ "/"
